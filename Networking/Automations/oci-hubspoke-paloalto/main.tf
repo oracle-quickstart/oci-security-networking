b3138cbd9399b44556128a3775ff4abf1c024a7e
Vinoth Kumar Ashok <reachvinohere@gmail.com>
2025-07-11 18:12:12 +0530

    Add files via upload

---
+++ Networking/Automations/oci-hubspoke-paloalto/main.tf
@@ -0,0 +1,181 @@
+#DRG
+#==================================================
+resource "oci_core_drg" "drg01" {
+  compartment_id = var.compartment_id
+  display_name  = "DRG01"
+}
+#DRG ATTACHMENT
+#==================================================
+resource "oci_core_drg_attachment" "hub_attachment" {
+  depends_on = [ oci_core_route_table.fw_route_table ]
+  drg_id     = oci_core_drg.drg01.id
+  display_name = "hub-attachment"
+  vcn_id = oci_core_vcn.HUB-VCN.id
+  route_table_id = oci_core_route_table.fw_route_table.id  
+  drg_route_table_id = oci_core_drg_route_table.hub_drg_route_table.id
+}
+resource "oci_core_drg_attachment" "spoke_drg_attachment" {
+  for_each = var.spoke_vcn
+  drg_id       = oci_core_drg.drg01.id
+  vcn_id       = oci_core_vcn.spoke_vcn[each.key].id
+  display_name = "${each.key}-drg-attachment"
+  drg_route_table_id = oci_core_drg_route_table.spoke_drg_route_table[each.key].id
+}
+#DRG SPOKE ROUTE-TABLE
+#==================================================
+resource "oci_core_drg_route_table" "spoke_drg_route_table" {
+  for_each = var.spoke_vcn
+    drg_id = oci_core_drg.drg01.id
+    display_name = "${each.key}_drg_route_table"
+    
+}
+resource "oci_core_drg_route_table_route_rule" "spoke_drg_route_table_rule" {
+  for_each = var.spoke_vcn
+  depends_on = [oci_core_drg_attachment.hub_attachment]
+    #Required
+    drg_route_table_id = oci_core_drg_route_table.spoke_drg_route_table[each.key].id
+    destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    next_hop_drg_attachment_id = oci_core_drg_attachment.hub_attachment.id
+}
+#DRG HUB ROUTE-TABLE
+#==================================================
+resource "oci_core_drg_route_table" "hub_drg_route_table" {
+    drg_id = oci_core_drg.drg01.id
+    display_name = "hub_drg_route_table"
+}
+resource "oci_core_drg_route_table_route_rule" "hub_drg_route_table_rule" {
+    for_each = var.spoke_vcn
+    #Required
+    drg_route_table_id = oci_core_drg_route_table.hub_drg_route_table.id
+    destination = each.value.cidr_block
+    destination_type = "CIDR_BLOCK"
+    next_hop_drg_attachment_id = oci_core_drg_attachment.spoke_drg_attachment[each.key].id
+}
+resource "oci_core_route_table" "fw_route_table" { 
+ depends_on = [ oci_core_instance.palo_alto, oci_core_vnic_attachment.trust_vnic ]
+ compartment_id = var.compartment_id
+ vcn_id         = oci_core_vcn.HUB-VCN.id
+ display_name   = "fw_route_table"
+ route_rules {
+ network_entity_id = data.oci_core_private_ips.pan_trust_subnet_private_ip.private_ips[0].id
+ description    = "route-traffic-to-fw"
+ destination    = "0.0.0.0/0"
+ destination_type = "CIDR_BLOCK"
+ }
+ }
+#HUB VCN deployement
+#==================================================
+resource "oci_core_vcn" "HUB-VCN" {
+  compartment_id = var.compartment_id
+  cidr_block     = var.hub_vcn_cidr
+  display_name   = "HUB-VCN"
+  dns_label = "hubvcn"
+}
+
+resource "oci_core_subnet" "pan_mgmt_subnet" {
+  depends_on = [ oci_core_security_list.hub_security_list ]
+  compartment_id      = var.compartment_id
+  cidr_block          = var.pan_mgmt_subnet
+  vcn_id              = oci_core_vcn.HUB-VCN.id
+  display_name        = "pan_mgmt_subnet"
+  dns_label           = "panmgmtsubnet"
+  route_table_id = oci_core_route_table.hub_route_table_internet.id
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  prohibit_public_ip_on_vnic = false
+  security_list_ids = [oci_core_security_list.hub_security_list.id]
+}
+
+resource "oci_core_subnet" "pan_untrust_subnet" {
+  compartment_id      = var.compartment_id
+  vcn_id              = oci_core_vcn.HUB-VCN.id
+  display_name        = "pan_untrust_subnet"
+  dns_label           = "panuntrust"
+  cidr_block          = var.pan_untrust_subnet
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+}
+
+resource "oci_core_subnet" "pan_trust_subnet" {
+  compartment_id      = var.compartment_id
+  vcn_id              = oci_core_vcn.HUB-VCN.id
+  display_name        = "pan_trust_subnet"
+  dns_label           = "pantrustsubnet"
+  cidr_block          = var.pan_trust_subnet
+  route_table_id = oci_core_route_table.trust_subnet_routetable.id
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  security_list_ids = [oci_core_security_list.trust_security_list.id]
+}
+
+
+resource "oci_core_security_list" "hub_security_list" {
+  compartment_id = var.compartment_id
+  display_name   = "HUB-SL"
+  vcn_id = oci_core_vcn.HUB-VCN.id 
+  ingress_security_rules {
+    protocol    = "all"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+  }
+  egress_security_rules {
+    protocol    = "all"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+  }
+}
+resource "oci_core_route_table" "hub_route_table_internet" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.HUB-VCN.id
+  display_name   = "hub_public_route_table"
+  # Internet Gateway rule
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.hub_internet_gateway.id
+    description       = "internet-gateway-rule"
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+ 
+}
+resource "oci_core_internet_gateway" "hub_internet_gateway" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.HUB-VCN.id
+  enabled        = var.internet_gateway_enabled
+  display_name   = "hub_internet_gateway"
+}
+resource "oci_core_route_table" "trust_subnet_routetable" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.HUB-VCN.id
+  display_name   = "trust_subnet_routetable"  
+  # Dynamic rules to route to spoke VCNs
+  dynamic "route_rules" {
+    for_each = var.spoke_vcn
+    content {
+      network_entity_id = oci_core_drg.drg01.id
+      description        = "route traffic to ${route_rules.key}"
+      destination        = route_rules.value.cidr_block
+      destination_type   = "CIDR_BLOCK"
+    }
+  }
+}
+resource "oci_core_security_list" "trust_security_list" {
+  compartment_id = var.compartment_id
+  display_name   = "trust-securitylist"
+  vcn_id = oci_core_vcn.HUB-VCN.id 
+  ingress_security_rules {
+    protocol    = "all"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+  }
+  egress_security_rules {
+    protocol    = "all"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+  }
+}
+data "oci_core_private_ips" "pan_trust_subnet_private_ip" { 
+  depends_on = [ oci_core_instance.palo_alto, oci_core_vnic_attachment.trust_vnic ]
+  subnet_id = oci_core_subnet.pan_trust_subnet.id
+  filter {
+    name   = "display_name"
+    values = ["trust_vnic"]
+  }
+}
---
+++ Networking/Automations/oci-hubspoke-paloalto/spoke.tf
@@ -0,0 +1,106 @@
+# VCN Resource
+resource "oci_core_vcn" "spoke_vcn" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  cidr_block     = each.value.cidr_block
+  display_name   = each.key
+  dns_label      = each.value.dns_label
+}
+# Public Subnet for each Spoke
+resource "oci_core_subnet" "public_subnet" {
+  depends_on = [ oci_core_security_list.spoke_security_list ]
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  cidr_block     = each.value.public_subnet_cidr
+  display_name   = "${each.key}-public"
+  dns_label      = "${each.key}public"
+  route_table_id = oci_core_route_table.spoke_public_route_table[each.key].id
+  security_list_ids = [oci_core_security_list.spoke_security_list[each.key].id]
+}
+# Private Subnet for each Spoke
+resource "oci_core_subnet" "private_subnet" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  cidr_block     = each.value.private_subnet_cidr
+  display_name   = "${each.key}-private"
+  dns_label      = "${each.key}private"
+  route_table_id = oci_core_route_table.spoke_private_route_table[each.key].id
+  security_list_ids = [oci_core_security_list.spoke_security_list[each.key].id]
+}
+resource "oci_core_security_list" "spoke_security_list" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  display_name   = "${each.key}-SL"
+  # Allow ICMP only from other spokes
+  dynamic "ingress_security_rules" {
+    for_each = {
+      for k, v in var.spoke_vcn : k => v.cidr_block
+      if k != each.key
+    }
+    content {
+      protocol    = "1"
+      source      = ingress_security_rules.value
+      source_type = "CIDR_BLOCK"
+    }
+  }
+  # Allow TCP from anywhere (e.g., SSH, HTTP)
+  ingress_security_rules {
+    protocol    = "6"
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+  }
+  egress_security_rules {
+    protocol    = "all"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+  }
+}
+# Route Table for Public Subnet (per spoke)
+resource "oci_core_route_table" "spoke_public_route_table" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  display_name   = "${each.key}-public-route-table"
+  route_rules {
+    network_entity_id = oci_core_drg.drg01.id
+    description       = "internet-gateway-rule"
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+  route_rules  {
+      destination       = "${var.my_public_ip}/32"
+      destination_type  = "CIDR_BLOCK"
+      network_entity_id = oci_core_internet_gateway.spoke_igw[each.key].id 
+      description       = "Route to my desktop public IP"
+    }
+}
+resource "oci_core_route_table" "spoke_private_route_table" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  display_name   = "${each.key}-private-route-table"
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_drg.drg01.id
+    description       = "Private subnet route to DRG"
+  }
+}
+# Internet Gateway per Spoke
+resource "oci_core_internet_gateway" "spoke_igw" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  enabled        = var.internet_gateway_enabled
+  display_name   = "${each.key}-igw"
+}
+# NAT Gateway for each Spoke (optional, depending on the need)
+resource "oci_core_nat_gateway" "spoke_nat_gateway" {
+  for_each = var.spoke_vcn
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.spoke_vcn[each.key].id
+  display_name   = "${each.key}-nat-gw"
+}
